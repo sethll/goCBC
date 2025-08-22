6e41e5793a90709288aa5c3d639bdf779c006407
sethll <sethll@users.noreply.github.com>
2025-08-21 22:07:21 -0700

    updated license

--- cmd/goCBC/main.go
+++ cmd/goCBC/main.go
@@ -1,8 +1,25 @@
 package main
 
+/*
+	goCBC
+	Copyright (C) 2025  Seth L
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import (
 	"fmt"
-	"log/slog"
 	"os"
 
 	"github.com/charmbracelet/log"
@@ -10,6 +27,10 @@ import (
 	"github.com/sethll/goCBC/pkg/progmeta"
 	"github.com/sethll/goCBC/pkg/progutils"
 	"github.com/spf13/cobra"
+)
+
+import (
+	"log/slog"
 )
 
 var (
